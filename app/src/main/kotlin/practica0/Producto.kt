e0292b5d3a78d83e9c1de9158fb2bd44ceed8088
Enrique de Ridder <enriquederidder5@gmail.com>
2023-10-04 20:28:31 +0200

    Initial commit

---
+++ app/src/main/kotlin/practica0/Producto.kt
@@ -0,0 +1,14 @@
+package practica0
+
+enum class Estado{
+    ACTIVO,
+    INACTIVO
+}
+class Producto(
+    var nombre: String,
+    var precio: Float,
+    var imagen: String,
+    var caracteristicas: String,
+    var estado: Estado = Estado.ACTIVO
+
+)
